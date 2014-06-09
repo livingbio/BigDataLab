b8fc13744b8f66eabd3b00c87db1e455b2aa168a
David Chen <lucemia@gmail.com>
2014-06-09 18:51:40 +0800

    update RestFul

--- appengine_config.py
+++ appengine_config.py
@@ -4,5 +4,7 @@ import google
 
 lib_dir = os.path.join(os.path.dirname(__file__), 'lib')
 endpoints = os.path.join(os.path.dirname(__file__), 'lib/endpoints-proto-datastore')
+endpoints_rest = os.path.join(os.path.dirname(__file__), 'lib/endpoints-proto-datastore-rest')
 sys.path.insert(0, lib_dir)
 sys.path.insert(0, endpoints)
+sys.path.insert(0, endpoints_rest)
--- generic_pipeline/apis.py
+++ generic_pipeline/apis.py
@@ -1,6 +1,11 @@
 import endpoints
 from .models import *
 from protorpc import remote
-from lib.endpoints_restful import *
+from endpoints_proto_datastore_rest import *
 
-BigDataLab = EndpointRestBuilder(GPCode).build(name="BigDataLab", version="v1", description="My Little Api")
+BigDataLab = EndpointRestBuilder(GPCode).build(
+    api_name="BigDataLab",
+    name="bigdatalab",
+    version="v1",
+    description="My Little Api"
+)
